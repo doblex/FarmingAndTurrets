1947180be237dcc476388d708204f1f32d9ea91c
Matteo Golfetto <Matteogol16@gmail.com>
2026-02-11 19:47:33 +0100

    First Push

---
+++ Source/FarmerAndTurrets/Actors/GameplayTagsActor.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagAssetInterface.h"
+#include "GameFramework/Actor.h"
+#include "GameplayTagsActor.generated.h"
+
+UCLASS()
+class FARMERANDTURRETS_API AGameplayTagsActor : public AActor, public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGameplayTagsActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Gameplay Tags")
+	FGameplayTag GameplayTag;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const;
+};
---
+++ Source/FarmerAndTurrets/FarmerAndTurrets.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FarmerAndTurrets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FarmerAndTurrets, "FarmerAndTurrets" );
